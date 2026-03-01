be77caa3778bffd26dde71818bf4c0c7dbfee90f
Pablo Leonardo Getar <pablo.leonardo.getar@gmail.com>
2026-03-01 19:03:59 -0300

    [test] modifying image send method to pyperclip
    Due to changes in sending image methods in Whatsapp web, this version is testing the pytautogui + pyperclip to send the image path as paste from clipboard

--- whatsappweb_objects.py
+++ whatsappweb_objects.py
@@ -1,11 +1,21 @@
 objects = {
-    'send_btn': '[aria-label=\"Enviar\"]',
+    'send_btn': '#main > footer > div.x1n2onr6.xhtitgo.x9f619.x78zum5.x1q0g3np.xuk3077.xjbqb8w.x1wiwyrm.xquzyny.xvc5jky.x11t971q.xnpuxes.copyable-area > div > span > div > div._ak1r > div > div.x9f619.x78zum5.x6s0dn4.xl56j7k.xpvyfi4.x2lah0s.x1c4vz4f.x1fns5xo.x1ba4aug.x1c9tyrk.xeusxvb.x1pahc9y.x1ertn4p.x1pse0pq.xpcyujq.xfn3atn.x1ypdohk.x1m2oepg > div > span > button > div > div > div:nth-child(1) > span',
+    'wrong_number_wrng': "[aria-label^='O número']", # starting a CCS with ^= searchs by left substring
+    'wrong_number_continue_btn': '#app > div > div > span:nth-child(3) > div > span > div > div > div > div > div > div.x78zum5.x8hhl5t.x13a6bvl.x13crsa5.x1gabggj.x18d9i69.xaso8d8.xp4054r.xuxw1ft > div',
+    'add_attachment_btn': '[data-icon=\"plus-rounded\"]',
+    'send_attachment_btn': '[data-icon=\"wds-ic-send-filled\"]',
+    'pictures_and_videos_btn': '#app > div > div > div:nth-child(11) > div > div > div.xu96u03.xm80bdy.x10l6tqk.x13vifvy.xoz0ns6.x1gslohp > div.html-div.xdj266r.x14z9mp.xat24cr.x1lziwak.xexx8yu.xyri2b.x18d9i69.x1c1uobl > div > div > div > div > div.x78zum5.xdt5ytf.x1iyjqo2.x1n2onr6 > div:nth-child(2)'
+}
+
+# previous
+# 'pictures_and_videos_btn': '//*[@id="app"]/div[1]/span[6]/div/ul/div/div/div[2]/li/div/input'
+
+"""
+    # 'send_btn': '[aria-label=\"Enviar\"]',
+    'send_btn': '#main > footer > div.x1n2onr6.xhtitgo.x9f619.x78zum5.x1q0g3np.xuk3077.xjbqb8w.x1wiwyrm.xquzyny.xvc5jky.x11t971q.xnpuxes.copyable-area > div > span > div > div._ak1r > div > div.x9f619.x78zum5.x6s0dn4.xl56j7k.xpvyfi4.x2lah0s.x1c4vz4f.x1fns5xo.x1ba4aug.x1c9tyrk.xeusxvb.x1pahc9y.x1ertn4p.x1pse0pq.xpcyujq.xfn3atn.x1ypdohk.x1m2oepg > div > span > button > div > div > div:nth-child(1) > span',
     'wrong_number_btn': '[aria-label=\'O número de telefone compartilhado por url é inválido.\']',
     'wrong_number_continue_btn': '#app > div._aiwn._aiwm.app-wrapper-web.font-fix.os-win.x1h89ln0.xoeyzqq > span:nth-child(3) > div > span > div > div > div > div > div > div.x78zum5.x8hhl5t.x13a6bvl.x13crsa5.x1gabggj.x18d9i69.xaso8d8.xp4054r.xuxw1ft > div > div > div > div.html-div.xdj266r.x14z9mp.xat24cr.x1lziwak.xexx8yu.x18d9i69.x78zum5.xl56j7k.x1ejq31n.x18oe1m7.x1sy0etr.xstzfhl.x1so62im.x1syfmzz.x1ja2u2z.x1s928wv.x1j6awrg.x4eaejv.x1wsn0xg.x1r0yslu.x2q1x1w.xapdjt.xr6f91l.x5rv0tg.x1akc3lz.xikp0eg.x1xl5mkn.x1mfml39.x1l5mzlr.xgmdoj8.x1f1wgk5.x1x3ic1u.xfn3atn.x1pse0pq.x1yxkqql.x9f619.xt8t1vi.x1xc408v.x129tdwq.x15urzxu.x1vqgdyp.x4m7ku4.xyo0t3i.xb0esv5 > div > span',
     'add_attachment_btn': '[data-icon=\"plus-rounded\"]',
     'send_attachment_btn': '[data-icon=\"wds-ic-send-filled\"]',
     'pictures_and_videos_btn': '//*[@id="app"]/div[1]/div/span[6]/div/ul/div/div/div[2]/li/div/input'
-}
-
-# previous
-# 'pictures_and_videos_btn': '//*[@id="app"]/div[1]/span[6]/div/ul/div/div/div[2]/li/div/input'
+"""
